7677797f5ef4f6be892e072a97ac0f3aff0c9e4b
ZakCstGame <39934153+ZakCstGame@users.noreply.github.com>
2018-06-06 06:47:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -338,7 +338,9 @@ bot.on("message", async message => {
 			return;
 		case "unmute":
 			if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("Je comprend mais il te faut quand meme quelques permissions pour faire cela ..");
+			let toMute = message.mentions.members.first() || message.guild.members.get(args[0]);
 			if(!toMute) return message.channel.sendMessage("Il faut mentionner quelqu'un Bro");
+			let role = message.guild.roles.find(r => r.name === "TG Tu es Muted");
 			if(!role || !toMute.roles.has(role.id)) return message.channel.sendMessage("Il est deja Muted, tu veux quoi de plus Enfwarey !?");
 			await toMute.removeRole(role);
 			message.channel.sendMessage("c'est fait, Je l'ai unMute !");
