f550073d73ed0af0887e318712d98ec282f57c45
ZakCstGame <39934153+ZakCstGame@users.noreply.github.com>
2018-06-13 18:21:42 +0200

    Update Aide.js

--- LesCommandes/Aide.js
+++ LesCommandes/Aide.js
@@ -3,13 +3,13 @@ const Discord = module.require('discord.js');
 module.exports.run = async (bot, message, args) =>{
 
 	let help_embed = new Discord.RichEmbed()
-		.setColor(0xFFC256)
+		.setColor(0xFFCCFF)
 		.setThumbnail(message.author.avatarURL)
-		.setDescription("Je suis le meilleur bot, ramene moi n'importe quelle machine tu verra ..")
-		.addField("Voici les commandes du bot :", ".aide pour afficher les commandes du bot !")
-		.addField("INTERACTiONS :", ".zak suivis d'une question ?")
-		.addField("*Info", "Le bot vous envoie des informations sur votre profile")
-		.setFooter("Menu d'aide est en cours de reglage .. relax enfwarey")
+		.setDescription("Hey bro, voici les commandes du moment : _(ATTENTiON AUX MAJUSCULES ET LE PREFiX C'EST UN POiNT . )_ ")
+		.addField(".Aide :", "C'est pour afficher les commandes du bot !")
+		.addField(".MonAvatar :", "C'est pour afficher l'image de ton avatar")
+		.addField(".Moi :", "C'est pour découvrir les informations de ton profile")
+		.setFooter("_Menu d'aide est en cours de reglage .. relax enfwarey_")
 	message.channel.sendMessage(help_embed);
 }
 
