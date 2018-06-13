84eee3a4481bb3af6abcc4f4f96740cfbb30d527
ZakCstGame <39934153+ZakCstGame@users.noreply.github.com>
2018-06-13 20:09:07 +0200

    Update Aide.js

--- LesCommandes/Aide.js
+++ LesCommandes/Aide.js
@@ -8,8 +8,11 @@ module.exports.run = async (bot, message, args) =>{
 		.setDescription("Hey bro, voici les commandes du moment : _(ATTENTiON AUX MAJUSCULES ET LE PREFiX C'EST UN POiNT . )_ ")
 		.addField(".Aide :", "C'est pour afficher les commandes du bot !")
 		.addField(".MonAvatar :", "C'est pour afficher l'image de ton avatar")
-		.addField(".Moi :", "C'est pour découvrir les informations de ton profile")
-		.setFooter("_Menu d'aide est en cours de reglage ... relax enfwarey_")
+		.addField(".Icon :", "C'est pour charger l'icone du Serveur")
+		.addField(".Mute :", "C'est pour mettre quelqu'un en Sourdine, _iL FAUT AVOiR LES PERMiSSiONS ET MENTiONNER L'UTiLiSATEUR_")
+		.addField(".UnMute :", "C'est pour reactiver quelqu'un en Sourdine, _iL FAUT AVOiR LES PERMiSSiONS ET MENTiONNER L'UTiLiSATEUR_")
+		.addField(".Moi :", "C'est pour decouvrir les informations de ton profile")
+		.setFooter("Menu d'aide est en cours de reglage .. relax enfwarey")
 	message.channel.sendMessage(help_embed);
 }
 
